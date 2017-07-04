ad7370dd4c83ed27fed5213c5070250d8660c8ce
Allan Henrique MRodrigues <p000alrodrigues@prservicos.com.br>
2017-07-04 20:52:43 -0300

    merge master com motor_allan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+   changeListener($event) : void {
+    this.readThis($event.target);
+  }
+
+  readThis(inputValue: any) : void {
+    var file:File = inputValue.files[0]; 
+    var myReader:FileReader = new FileReader();
+
+    myReader.onloadend = function(e){
+      // you can perform an action with readed data here
+      console.log(myReader.result);
+    }
+
+    myReader.readAsText(file);
+  }
 }

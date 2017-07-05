d52a1900e8c2ff208f59a8d6553b1516c2d6210a
Fernando Marcos Freitag <p000ffreitag@prservicos.com.br>
2017-07-05 14:43:55 -0300

    Upload e leitura do arquivo xhtml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +7,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+   public fileString;
+
+    constructor() {
+        this.fileString;
+    }
+
+    changeListener($event): void {
+            this.readThis($event.target);
+        }
+
+    readThis(inputValue: any): void {
+        var file: File = inputValue.files[0];
+        var myReader: FileReader = new FileReader();
+        var fileType = inputValue.parentElement.id;
+        myReader.onloadend = function (e) {
+            //myReader.result is a String of the uploaded file
+            console.log(myReader.result);
+            
+            //fileString = myReader.result would not work, 
+            //because it is not in the scope of the callback
+        }
+        
+       myReader.readAsText(file);
+       this.convMain(myReader.result);
+    }
+
+    convMain(strHtml : string) : void {
+      debugger;
+      var strTag : string;
+      var posicao : number = 0;
+      var strHtml : string;
+      
+    
+      switch (strTag) {
+        case "input" : 
+          this.convInput(strHtml);
+          break;
+        case "label" : 
+          this.convLabel(strHtml);
+          break;
+        case "div" : 
+          this.convDiv(strHtml);
+          break;
+        case "button" :
+          break; 
+       
+      }
+    } 	
+
+    convInput(html : string) : string {
+      return "";
+    }
+
+    convLabel(html : string) : string {
+      return "";
+    }
+
+    convDiv(html : string) : string {
+      return "";
+    }
+
+    convButton(html : string) : string {
+      return "";
+    }
 }

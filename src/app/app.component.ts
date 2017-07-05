43e021d1aceb3065f48c81a7501ba5e3a850d497
Allan Henrique MRodrigues <p000alrodrigues@prservicos.com.br>
2017-07-05 15:23:08 -0300

    tratamento para receber varios arquivos de uma vez

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,26 +18,32 @@ export class AppComponent {
         }
 
     readThis(inputValue: any): void {
-        var file: File = inputValue.files[0];
+        // var file: File = inputValue.files[0];
+        var files : File[] = inputValue.files;
         var myReader: FileReader = new FileReader();
         var fileType = inputValue.parentElement.id;
         myReader.onloadend = function (e) {
             //myReader.result is a String of the uploaded file
-            console.log(myReader.result);
+            // console.log(myReader.result);
             
             //fileString = myReader.result would not work, 
             //because it is not in the scope of the callback
         }
+
+        files.forEach(element => {
+          myReader.readAsText(element);
+          this.convMain(myReader.result);
+        });
         
-       myReader.readAsText(file);
-       this.convMain(myReader.result);
+      //  myReader.readAsText(file);
+      //  this.convMain(myReader.result);
     }
 
     convMain(strHtml : string) : void {
       debugger;
       var strTag : string;
       var posicao : number = 0;
-      var strHtml : string;
+      // var strHtml : string;
       
     
       switch (strTag) {

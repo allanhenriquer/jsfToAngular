876463d8ebe5ebe6a0136cb597f964a04d13f1ec
Allan Henrique MRodrigues <p000alrodrigues@prservicos.com.br>
2017-07-06 12:33:32 -0300

    ligacao do leitor de arquivos com o motor principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,73 +7,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-   public fileString;
+  public fileString;
 
-    constructor() {
-        this.fileString;
-    }
-
-    changeListener($event): void {
-            this.readThis($event.target);
-        }
-
-    readThis(inputValue: any): void {
-        var file: File = inputValue.files[0];
-        // var files : File[] = inputValue.files;
-        var myReader: FileReader = new FileReader();
-        var fileType = inputValue.parentElement.id;
-        myReader.onloadend = function (e) {
-            console.log(myReader.result);
-        }
-
-        // files.forEach(element => {
-        //   myReader.readAsText(element);
-        //   this.convMain(myReader.result);
-        // });
-        
-       myReader.readAsText(file);
-       this.convMain(myReader.result);
-       console.log("passei aqui!");
-    }
-
-    convMain(strHtml : string) : void {
-      debugger;
-      var strTag : string;
-      var posicao : number = 0;
-      // var strHtml : string;
-      
-    
-      switch (strTag) {
-        case "input" : 
-          this.convInput(strHtml);
-          console.log('depois');
-          break;
-        case "label" : 
-          this.convLabel(strHtml);
-          break;
-        case "div" : 
-          this.convDiv(strHtml);
-          break;
-        case "button" :
-          break; 
-       
-      }
-    } 	
-
-    convInput(html : string) : string {
-      console.log('antes');
-      return html && html.replace('p:outputPanel', 'teste');
+  constructor() {
+    this.fileString;
   }
 
-    convLabel(html : string) : string {
-      return "";
+  changeListener($event): void {
+    this.readThis($event.target);
+  }
+
+  readThis(inputValue: any): void {
+    debugger;
+    var file: File = inputValue.files[0];
+    var myReader: FileReader = new FileReader();
+    myReader.onloadend = function (e) {
+      console.log("ainda nao carregou");
+      console.log("ja carregou");
+      AppComponent.prototype.convMain(myReader.result);
     }
 
-    convDiv(html : string) : string {
-      return "";
-    }
+    myReader.readAsText(file);
+   // this.convMain(myReader.result);
+  }
 
-    convButton(html : string) : string {
-      return "";
+  convMain(strHtml: string): void {
+    console.log("entrou no metodo convMain");
+    console.log(strHtml);
+    var strTag: string;
+    var posicao: number = 0;
+    // var strHtml : string;
+
+
+    switch (strTag) {
+      case "input":
+        this.convInput(strHtml);
+        console.log('Final de switch - input');
+        break;
+      case "label":
+        this.convLabel(strHtml);
+        break;
+      case "div":
+        this.convDiv(strHtml);
+        break;
+      case "button":
+        break;
+
     }
+  }
+
+  convInput(html: string): string {
+    console.log('antes');
+    return html && html.replace('p:outputPanel', 'teste');
+  }
+
+  convLabel(html: string): string {
+    return "";
+  }
+
+  convDiv(html: string): string {
+    return "";
+  }
+
+  convButton(html: string): string {
+    return "";
+  }
 }

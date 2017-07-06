55fb0ff533e5d5387d5d8fe2b10f9cde0abf5f07
Andre P Oliveira <andresystems95@gmail.com>
2017-07-06 20:42:43 -0300

    fase inicial botão

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,8 @@ export class AppComponent {
         var fileType = inputValue.parentElement.id;
         myReader.onloadend = function (e) {
             //myReader.result is a String of the uploaded file
+
+           
             console.log(myReader.result);
             
             //fileString = myReader.result would not work, 
@@ -30,12 +32,18 @@ export class AppComponent {
         }
         
        myReader.readAsText(file);
-       this.convMain(myReader.result);
+  
+
+       var res = 'button';
+
+       var fs = 'ds';
+
+        this.convMain(res+"");
     }
 
     convMain(strHtml : string) : void {
       debugger;
-      var strTag : string;
+      var strTag : string = strHtml;
       var posicao : number = 0;
       var strHtml : string;
       
@@ -51,6 +59,7 @@ export class AppComponent {
           this.convDiv(strHtml);
           break;
         case "button" :
+          this.convButton(strHtml);
           break; 
        
       }
@@ -69,6 +78,74 @@ export class AppComponent {
     }
 
     convButton(html : string) : string {
-      return "";
+
+    html = '<p:commandLink value="Ajax Submit"></p:commandLink>'
+
+    var regxValueApas = /value\s*=*\s*(["'])(?:(?=(\\?))\2.)*?\1/; //identifica value="conteudo"
+    var pegaVal = /value\s*/ ;
+    var regxValue = /value\s*=*\s*/; //identifica value=
+    var regxStyleApas = /style\s*=*\s*(["'])(?:(?=(\\?))\2.)*?\1/; //identifica style="conteudo"
+    var regxStyle = /style\s*=*\s*/; //identifica style=
+    
+
+
+    if (html.match(regxValueApas) != null) { //verifica se tem value
+
+      var valorTagValue = this.pegaConteudo(html, regxValueApas, regxValue);
+    
+    
+      var valorTag = this.pegaConteudoTag(html, regxValueApas, pegaVal);
+
+      var tagCorreta = "<button "+ valorTag + "='" + valorTagValue + "'></button>"
+
+      console.log(tagCorreta);
+
+      // if (html.match(regxStyleApas) != null) { //verifica se tem style
+
+      //   var valorTagStyle = this.pegaConteudo(html, regxStyleApas, regxStyle);
+
+      //   html = '<label style="' + valorTagStyle + '">' + valorTagValue + '</label>';
+      //   console.log(html);
+
+      // }  
+
+    } else {
+
+      html = "<label>" + "</label>";
+      console.log(html);
+
     }
+    return "";
+  }
+
+  pegaConteudo(html: string, regxToFindAspas, regxToFind): string {
+
+    var regxEspaco = /\s/g;
+
+    var pegaValue = regxToFind.exec(html);
+    pegaValue[0] = pegaValue[0].replace(regxEspaco, "");
+    var html = html.replace(regxToFind, pegaValue[0]);
+
+    var novoRegx = regxToFindAspas.exec(html);
+    var indexValue = novoRegx[0].indexOf("value");
+
+     var sbStrValue = novoRegx[0].substring(indexValue + 7, novoRegx[0].length - 1);
+     
+
+
+    return sbStrValue;
+    
+}
+
+  pegaConteudoTag(html: string, regxToFindAspas, regxToFind): string {
+
+    var regxEspaco = /\s/g;
+
+    var pegaValue = regxToFind.exec(html);
+  
+
+    return pegaValue;
+    
+}
+
 }

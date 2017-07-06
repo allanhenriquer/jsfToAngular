afef9455ecd1a8b4693f4530a480f75ceb8ddd64
Allan Henrique MRodrigues <p000alrodrigues@prservicos.com.br>
2017-07-06 07:53:04 -0300

    andamento do motor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,25 +18,22 @@ export class AppComponent {
         }
 
     readThis(inputValue: any): void {
-        // var file: File = inputValue.files[0];
-        var files : File[] = inputValue.files;
+        var file: File = inputValue.files[0];
+        // var files : File[] = inputValue.files;
         var myReader: FileReader = new FileReader();
         var fileType = inputValue.parentElement.id;
         myReader.onloadend = function (e) {
-            //myReader.result is a String of the uploaded file
-            // console.log(myReader.result);
-            
-            //fileString = myReader.result would not work, 
-            //because it is not in the scope of the callback
+            console.log(myReader.result);
         }
 
-        files.forEach(element => {
-          myReader.readAsText(element);
-          this.convMain(myReader.result);
-        });
+        // files.forEach(element => {
+        //   myReader.readAsText(element);
+        //   this.convMain(myReader.result);
+        // });
         
-      //  myReader.readAsText(file);
-      //  this.convMain(myReader.result);
+       myReader.readAsText(file);
+       this.convMain(myReader.result);
+       console.log("passei aqui!");
     }
 
     convMain(strHtml : string) : void {
@@ -49,6 +46,7 @@ export class AppComponent {
       switch (strTag) {
         case "input" : 
           this.convInput(strHtml);
+          console.log('depois');
           break;
         case "label" : 
           this.convLabel(strHtml);
@@ -63,8 +61,9 @@ export class AppComponent {
     } 	
 
     convInput(html : string) : string {
-      return "";
-    }
+      console.log('antes');
+      return html && html.replace('p:outputPanel', 'teste');
+  }
 
     convLabel(html : string) : string {
       return "";
